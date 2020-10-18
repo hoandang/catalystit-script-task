4dcd66f5a46733d8ea4705d21106933c2eed9f24
Hoan Dang <danghuyhoan@gmail.com>
2020-10-18 12:25:05 +1100

    Create database if not exists

--- user_upload.php
+++ user_upload.php
@@ -6,40 +6,72 @@ require_once 'vendor/autoload.php';
 use Garden\Cli\Cli;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+// Show help menu
 $cli = new Cli;
 $cli
   ->opt('file', '[csv file name] – this is the name of the CSV to be parsed')
   ->opt('create_table', 'this will cause the MySQL users table to be built (and no further action will be taken)')
   ->opt('dry_run', "this will be used with the --file directive in case we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered")
-  ->opt('db-host:h', 'MySQL host', true) 
-  ->opt('db-username:u', 'MySQL username', true) 
-  ->opt('db-password:p', 'MySQL password', true);
+  ->opt('db_host:h', 'MySQL host', true) 
+  ->opt('db_username:u', 'MySQL username', true) 
+  ->opt('db_password:p', 'MySQL password', true);
 
 // Parse and return cli args.
 $args = $cli->parse($argv, true);
 
-$DB_HOST = $args['db-host'];
-$DB_USERNAME = $args['db-username'];
-$DB_PASSWORD = $args['db-password'];
+// Capture user inputs
+$DB_HOST = $args->getOpt('db_host') == 'localhost' ? '127.0.0.1' : $args->getOpt('db_host');
+$DB_USERNAME = $args->getOpt('db_username');
+$DB_PASSWORD = $args->getOpt('db_password');
+
 $DB_NAME = 'CatalystIT';
 $DB_TABLE_NAME = 'Users';
 
-$capsule = new Capsule;
+// Bootstrap database if not exists
+function createDb()
+{
+  global $DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME;
+  $conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD);
+  if ($conn->connect_error) 
+  {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
+  // Create database
+  if ($conn->query("CREATE DATABASE IF NOT EXISTS $DB_NAME") !== TRUE) 
+  {
+    die("Error creating database: " . $conn->error);
+  }
+  $conn->close();
+}
+createDb();
+
+// Init  Illuminate database package for easy data manipulation
+$capsule = new Capsule;
 $capsule->addConnection([
   'driver' => 'mysql',
-  'host' => $DB_HOST,,
+  'host' => $DB_HOST,
   'database' => $DB_NAME,
-  'username' => $DB_USERNAME,,
+  'username' => $DB_USERNAME,
   'password' => $DB_PASSWORD
 ]);
 
+// bootstrap eloquent
 $capsule->setAsGlobal();
-
-// Setup the Eloquent ORM.
 $capsule->bootEloquent();
 
-// Init database if not existed
-$capsule->getConnection()->statement("CREATE DATABASE IF NOT EXISTS $DB_NAME");
+// Get connection 
+$dbConnection = $capsule->getConnection();
+$schema = Capsule::schema();
+
+// Check if user invokes create_table opt 
+if ($args->hasOpt('create_table') && !$schema->hasTable($DB_TABLE_NAME))
+{
+  $schema->create($DB_TABLE_NAME, function ($table) {
+    $table->increments('id');
+    $table->string('email')->unique();
+    $table->timestamps();
+  });
+}
 
 /* $user = Capsule::table('users')->where('id', 1)->get(); */
